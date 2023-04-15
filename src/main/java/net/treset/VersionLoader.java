4a232964767e76643714e3457b8d893a3ad37065
hanneshekl@gmail.com <hanneshekl@gmail.com>
2023-04-15 12:36:41 +0200

    Added release filtering

--- src/main/java/net/treset/VersionLoader.java
+++ src/main/java/net/treset/VersionLoader.java
@@ -1,13 +1,25 @@
 package net.treset;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class VersionLoader {
     public static void main(String[] args) {
-        System.out.println(getVersions());
+        System.out.println(getReleases());
     }
 
     public static List<Version> getVersions() {
         return JsonParser.parseVersionManifest(Sources.getVersionManifest());
+    }
+
+    public static List<Version> getReleases() {
+        List<Version> releases = new ArrayList<>();
+        List<Version> versions = getVersions();
+        for(Version v : versions) {
+            if(!v.isSnapshot()) {
+                releases.add(v);
+            }
+        }
+        return releases;
     }
 }
