8aa6267484e4cd428595a3706ebb956af29c6227
hanneshekl@gmail.com <hanneshekl@gmail.com>
2023-04-18 23:02:22 +0200

    Added downloaders, fixed things with launch arguments

--- src/main/java/net/treset/mc_version_loader/VersionLoader.java
+++ src/main/java/net/treset/mc_version_loader/VersionLoader.java
@@ -3,17 +3,48 @@ package net.treset.mc_version_loader;
 import net.treset.mc_version_loader.fabric.FabricLoaderData;
 import net.treset.mc_version_loader.fabric.FabricVersion;
 import net.treset.mc_version_loader.fabric.FabricVersionDetails;
+import net.treset.mc_version_loader.java.JavaFile;
 import net.treset.mc_version_loader.java.JavaManifest;
 import net.treset.mc_version_loader.java.JavaVersion;
 import net.treset.mc_version_loader.version.*;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
 public class VersionLoader {
     public static void main(String[] args) {
-        List<FabricVersion> versions = JsonParser.parseFabricManifest(Sources.getFabricForMinecraftVersion("1.19.4"));
-        FabricVersionDetails details = JsonParser.parseFabricVersion(Sources.getFabricVersion(versions.get(0).getMinecraftVersion(), versions.get(0).getLoaderVersion()));
+
+        List<Version> mcVersions = getReleases();
+        Version version = null;
+        for(Version v : mcVersions) {
+            if(v.getId().equals("1.19.4")) {
+                version = v;
+                break;
+            }
+        }
+        VersionDetails mcDetails = JsonParser.parseVersionDetails(Sources.getFileFromUrl(version.getUrl()));
+        List<FabricVersion> fabricVersions = JsonParser.parseFabricManifest(Sources.getFabricForMinecraftVersion("1.19.4"));
+
+        JavaVersion javaVersion = JsonParser.parseJavaVersion(Sources.getJavaRuntimeJson(), mcDetails.getJavaVersion().getComponent()).get(0);
+        JavaManifest javaManifest = JsonParser.parseJavaManifest(Sources.getFileFromUrl(javaVersion.getManifestUrl()));
+        File javaDir = new File("./downloads/java");
+        javaDir.mkdirs();
+        for(JavaFile f : javaManifest.getFiles()) {
+            FileDownloader.downloadJavaFile(f, javaDir);
+        }
+
+        File clientDir = new File("./downloads/client");
+        clientDir.mkdirs();
+        FileDownloader.downloadVersionDownload(mcDetails.getDownloads().getClient(), clientDir);
+        File libsDir = new File("./downloads/libraries");
+        libsDir.mkdirs();
+        for(VersionLibrary l : mcDetails.getLibraries()) {
+            FileDownloader.downloadVersionLibrary(l, libsDir);
+        }
+        VersionLaunchCommand launchCommand = mcDetails.getArguments().getLaunchCommand("java", "client.jar", mcDetails.getMainClass(), "../libraries" , mcDetails.getLibraries(), new ArrayList<>());
+        System.out.println(launchCommand.getLaunchCommand());
+        FabricVersionDetails details = JsonParser.parseFabricVersion(Sources.getFabricVersion(fabricVersions.get(0).getMinecraftVersion(), fabricVersions.get(0).getLoaderVersion()));
         return;
     }
 
--- src/main/java/net/treset/mc_version_loader/version/VersionArguments.java
+++ src/main/java/net/treset/mc_version_loader/version/VersionArguments.java
@@ -2,6 +2,7 @@ package net.treset.mc_version_loader.version;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class VersionArguments {
     private List<VersionArgument> game;
@@ -12,23 +13,42 @@ public class VersionArguments {
         this.jvm = jvm;
     }
 
-    public VersionLaunchCommand getLaunchCommand(String javaCommand, String gameLaunchFile, List<VersionFeature> features) {
+    public VersionLaunchCommand getLaunchCommand(String javaBaseDir, String gameLaunchFile, String mainClass, String libsDir, List<VersionLibrary> libraries, List<VersionFeature> features) {
+
+
         StringBuilder outString = new StringBuilder();
-        outString.append(javaCommand).append(" ");
+        outString.append(javaBaseDir).append("\\bin\\java ");
 
         List<String> outKeys = new ArrayList<>();
 
         for(VersionArgument a : getJvm()) {
-            addArgument(features, outString, outKeys, a);
+            if(a.isReplaced() && Objects.equals(a.getReplacementValue(), "classpath")) {
+                outString.append(buildClasspathString(gameLaunchFile, mainClass, libsDir, libraries));
+            } else {
+                addArgument(features, outString, outKeys, a);
+            }
         }
-
-        outString.append("-jar ").append(gameLaunchFile).append(" ");
 
         for(VersionArgument a : getGame()) {
             addArgument(features, outString, outKeys, a);
         }
 
         return new VersionLaunchCommand(outString.toString(), outKeys);
+    }
+
+    private String buildClasspathString(String gameLaunchFile, String mainClass, String libsDir, List<VersionLibrary> libraries) {
+        if(libsDir.endsWith("/")) {
+            libsDir = libsDir.substring(0, libsDir.length() - 1);
+        }
+        StringBuilder out = new StringBuilder();
+        out.append("\"").append(gameLaunchFile);
+        for(VersionLibrary l : libraries) {
+            if(l.getArtifactPath() != null && !l.getArtifactPath().isBlank()) {
+                out.append(";").append(libsDir).append("/").append(l.getArtifactPath());
+            }
+        }
+        out.append("\" ").append(mainClass).append(" ");
+        return out.toString();
     }
 
     private void addArgument(List<VersionFeature> features, StringBuilder outString, List<String> outKeys, VersionArgument a) {
