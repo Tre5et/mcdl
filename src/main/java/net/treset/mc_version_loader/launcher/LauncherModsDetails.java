57a80a740a85468fed37f914e793949c0a4dcb32
TreSet <hanneshekl@gmai.com>
2024-02-28 12:04:17 +0100

    Added Launcher File support for multiple loaders and versions

--- src/main/java/net/treset/mc_version_loader/launcher/LauncherModsDetails.java
+++ src/main/java/net/treset/mc_version_loader/launcher/LauncherModsDetails.java
@@ -1,39 +1,51 @@
 package net.treset.mc_version_loader.launcher;
 
+import com.google.gson.JsonObject;
 import net.treset.mc_version_loader.json.GenericJsonParsable;
+import net.treset.mc_version_loader.json.JsonUtils;
 import net.treset.mc_version_loader.json.SerializationException;
 
 import java.util.List;
 
 public class LauncherModsDetails extends GenericJsonParsable {
-    private String modsType;
-    private String modsVersion;
+    private List<String> types;
+    private List<String> versions;
     private List<LauncherMod> mods;
 
-    public LauncherModsDetails(String modsType, String modsVersion, List<LauncherMod> mods) {
-        this.modsType = modsType;
-        this.modsVersion = modsVersion;
+    public LauncherModsDetails(List<String> types, List<String> versions, List<LauncherMod> mods) {
+        this.types = types;
+        this.versions = versions;
         this.mods = mods;
     }
 
     public static LauncherModsDetails fromJson(String json) throws SerializationException {
-        return fromJson(json, LauncherModsDetails.class);
+        LauncherModsDetails details = fromJson(json, LauncherModsDetails.class);
+        if(details.getTypes() == null) {
+            JsonObject obj = JsonUtils.getAsJsonObject(JsonUtils.parseJson(json));
+            String type = JsonUtils.getAsString(obj, "mods_type");
+            details.setTypes(List.of(type));
+        }
+        if(details.getVersions() == null) {
+            JsonObject obj = JsonUtils.getAsJsonObject(JsonUtils.parseJson(json));            String version = JsonUtils.getAsString(obj, "mods_version");
+            details.setVersions(List.of(version));
+        }
+        return details;
     }
 
-    public String getModsType() {
-        return modsType;
+    public List<String> getTypes() {
+        return types;
     }
 
-    public void setModsType(String modsType) {
-        this.modsType = modsType;
+    public void setTypes(List<String> types) {
+        this.types = types;
     }
 
-    public String getModsVersion() {
-        return modsVersion;
+    public List<String> getVersions() {
+        return versions;
     }
 
-    public void setModsVersion(String modsVersion) {
-        this.modsVersion = modsVersion;
+    public void setVersions(List<String> versions) {
+        this.versions = versions;
     }
 
     public List<LauncherMod> getMods() {
