139ea96fbb118178451836eaee26e8a58e23233d
TreSet <hanneshekl@gmail.com>
2024-08-29 16:30:41 +0200

    Tests: Added Saves Test

---
+++ saves/src/test/java/TestSaves.java
@@ -0,0 +1,16 @@
+import net.treset.mcdl.saves.Save;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+public class TestSaves {
+    @ParameterizedTest
+    @ValueSource(strings = {"testfiles/parkour-spiral", "testfiles/tschui-iwa"})
+    public void testSave(String path) {
+        File file = new File(path);
+        assertDoesNotThrow(() -> Save.from(file), "cant parse save: " + file.getName());
+    }
+}
