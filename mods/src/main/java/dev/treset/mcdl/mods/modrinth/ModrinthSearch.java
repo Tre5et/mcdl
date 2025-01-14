33c359c6b5915d22acd96596bbf7be7f095d429a
TreSet <hanneshekl@gmail.com>
2025-01-14 14:33:42 +0100

    Change: Added Rate Limit logic to existing get method

--- mods/src/main/java/dev/treset/mcdl/mods/modrinth/ModrinthSearch.java
+++ mods/src/main/java/dev/treset/mcdl/mods/modrinth/ModrinthSearch.java
@@ -75,7 +75,7 @@ public class ModrinthSearch extends GenericJsonParsable {
             params.put(ModsDL.getModrinthSearchFacetsParam(), facets.toString());
         }
         try {
-            String content = HttpUtil.getStringRateLimited(ModsDL.getModrinthSearchUrl(), r -> 1000, ModsDL.getModrinthHeaders(ModsDL.getModrinthUserAgent()), params, ModsDL.getCaching());
+            String content = HttpUtil.getString(ModsDL.getModrinthSearchUrl(), ModsDL.getModrinthHeaders(ModsDL.getModrinthUserAgent()), params, ModsDL.getCaching());
             return ModrinthSearch.fromJson(content);
         } catch (SerializationException e) {
             throw new FileDownloadException("Unable to parse modrinth search results", e);
